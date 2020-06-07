6b4391fcf3cc26a9386dada2fc60ffb70eade13b
PSCBUS_1013 <pscbus_1013@PSCBUS-1013.columbus.rr.com>
2020-06-07 03:04:15 -0400

    first commit

---
+++ w2d2_hw.js
@@ -0,0 +1,23 @@
+//let str1 = "Fizz"
+//let str2  = "Buzz"
+/*let str3 = "FizzBuzz"
+
+for (let i = 0; i < 101; i++){
+    console.log(i)
+    if (i % 5 === 0 && i % 3 === 0){
+         console.log 'str3';
+        }else if (i % 3 === 0) {
+            console.log str1
+                }
+            }
+    }
+}
+*/
+
+
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+    console.log(plantee) 
+    console.log(plantee[2])
+
+//plantee.splice(2,1,5000 +1)
+  //  console.log (plantee)

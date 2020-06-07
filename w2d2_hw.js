87c49b6c28383be605344337ce715a34d6fd54dd
PSCBUS_1013 <pscbus_1013@PSCBUS-1013.columbus.rr.com>
2020-06-07 15:11:40 -0400

    Easy Going Answered

--- w2d2_hw.js
+++ w2d2_hw.js
@@ -1,24 +1,8 @@
-//let str1 = "Fizz"
-//let str2  = "Buzz"
-/*let str3 = "FizzBuzz"
+////////////////////////////////
+// Easy Going
+////////////////////////////////
 
-for (let i = 0; i < 101; i++){
-    console.log(i)
-    if (i % 5 === 0 && i % 3 === 0){
-         console.log 'str3';
-        }else if (i % 3 === 0) {
-            console.log str1
-                }
-            }
-    }
+for (let i = 0; i <21; i++){ //<----as long as my number called i, is less than 2, go up 1 increment and,
+    console.log (i) //<---- console log that number
 }
-*/
 
-
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-    console.log(plantee) 
-    console.log(plantee[2])
-
-plantee.splice(2,1,5000 +1)
-console.log (plantee)
-    

15091d7cf1069d9f7ca2e94ef94960ba64964a68
PSCBUS_1013 <pscbus_1013@PSCBUS-1013.columbus.rr.com>
2020-06-07 15:38:18 -0400

    Get Even Answered

--- w2d2_hw.js
+++ w2d2_hw.js
@@ -1,8 +1,18 @@
 ////////////////////////////////
 // Easy Going
 ////////////////////////////////
-
+/*
 for (let i = 0; i <21; i++){ //<----as long as my number called i, is less than 2, go up 1 increment and,
     console.log (i) //<---- console log that number
+}
+*/
+////////////////////////////////
+// Get Even
+////////////////////////////////
+
+for (let i = 0; i <201; i++){ //<----as long as my number called i, is less than 201, go up 1 increment and,
+    if (i % 2 === 0){ //<---- as long as the number is a divisible by 2 with no remainder,
+    console.log (i) //<---- console log that number
+    }
 }
 
